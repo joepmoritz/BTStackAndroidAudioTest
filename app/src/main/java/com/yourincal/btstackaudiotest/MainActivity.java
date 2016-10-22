c617c2af84eee31cba9e6683589800730ec726d1
Joep Moritz <joep.moritz@gmail.com>
2016-10-22 13:01:23 +0100

    Initial commit of new project.

---
+++ app/src/main/java/com/yourincal/btstackaudiotest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yourincal.btstackaudiotest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
